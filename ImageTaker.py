81c79f2cd2af0eef1de8653da5d5e3e50950b11a
unknown <Proyecto@H40-12.upc.edu.pe>
2023-04-04 18:50:18 -0500

    agregado nuevo video y fotos de natalia

--- ImageTaker.py
+++ ImageTaker.py
@@ -9,10 +9,10 @@ import os
 #os.mkdir(ruta_nueva_carpeta)
 
 # Ruta del video a procesar
-video_path = "./Videos/Elvis.mp4"
+video_path = "./Videos/Natalia.mp4"
 
 # Directorio donde se guardarán los frames extraídos del video
-frames_dir = "./caras_conocidas/Hamill"
+frames_dir = "./caras_conocidas/Natalia"
 
 # Crear directorio si no existe
 if not os.path.exists(frames_dir):
